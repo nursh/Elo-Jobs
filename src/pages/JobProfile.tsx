0436fb131eaf70c2a18d9b22d4c01af530d385cd
nur-sh <nsheikhmohammed01@gmail.com>
2024-12-20 01:23:16 -0500

    Refactor JobDetails into its own component

--- src/pages/JobProfile.tsx
+++ src/pages/JobProfile.tsx
@@ -1,7 +1,8 @@
 import { useParams } from "react-router"
-import { jobs } from "../utils/data";
 import { Header } from "../components/Header";
+import JobDetails from "../components/JobDetails";
 import '../assets/styles/JobProfile.css'
+import { jobs } from "../utils/data";
 
 type Params = {
   jobId: string;
@@ -14,35 +15,7 @@ export default function JobProfile() {
   return (
     <>
       <Header name="Job Profile" />
-      <div className="profile-heading">
-        <h4>{job.company}</h4>
-        <h2>{job.title}</h2>
-        <p>{job.location}</p>
-      </div>
-
-
-      <div className="profile-details">
-        <div className="profile-detail">
-          <h5>Required Gender:</h5>
-          <p>{job.requiredGender}</p>
-        </div>
-        <div className="profile-detail">
-          <h5>Required Age</h5>
-          <p>{job.requiredAge}</p>
-        </div>
-        <div className="profile-detail">
-          <h5>Employment Status:</h5>
-          <p>{job.requiredEmploymentStatus}</p>
-        </div>
-        <div className="profile-detail">
-          <h5>Languages:</h5>
-          <p>{job.requiredLanguages.join(', ')}</p>
-        </div>
-        <div className="profile-detail">
-          <h5>Citizenship:</h5>
-          <p>{job.requiredCitizenship}</p>
-        </div>
-      </div>
+      <JobDetails job={job} />
     </>
   )
 }
